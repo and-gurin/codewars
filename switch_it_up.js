809eac4cf89b755f1979ebb46123b1a7f7d67493
and-gurin <and.gurin@gmail.com>
2022-02-04 07:44:26 +0300

    feat: add task switch_it_up

---
+++ switch_it_up.js
@@ -0,0 +1,36 @@
+function switchItUp(number){
+    let answer = '';
+      switch (number) {
+          case 1:
+          answer = answer + 'One';
+          break;
+          case 2:
+          answer = answer + 'Two';
+          break;
+          case 3:
+          answer = answer + 'Three';
+          break;
+          case 4:
+          answer = answer + 'Four';
+          break;
+          case 5:
+          answer = answer + 'Five';
+          break;
+          case 6:
+          answer = answer + 'Six';
+          break;
+          case 7:
+          answer = answer + 'Seven';
+          break;
+          case 8:
+          answer = answer + 'Eight';
+          break;
+          case 9:
+          answer = answer + 'Nine';
+          break;
+          case 0:
+          answer = answer + 'Zero';
+          break;
+      }
+      return answer;
+    }
